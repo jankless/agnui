4d2a1c4d70b849c192571985fbd51b4e97e1b123
Warren Dugan <warrendugan@GMAIL.COM>
2022-01-13 11:37:04 -0800

    fix prod build

--- libs/nts/src/index.ts
+++ libs/nts/src/index.ts
@@ -1,2 +1,7 @@
+export * from './lib/contact/contact.component';
+export * from './lib/contact/contact.module';
+export * from './lib/footer/footer.component';
+export * from './lib/footer/footer.module';
+export * from './lib/header/header.component';
 export * from './lib/header/header.module';
 export * from './lib/nts.module';
